563596b419405ba0c185fe8c5fc6717350aa9e10
harald bregu <harald.bregu@gmail.com>
2026-03-07 21:09:33 +0100

    Remove evaluator node and evaluation fields from writing state

--- src/state.ts
+++ src/state.ts
@@ -1,11 +1,5 @@
 import { Annotation } from '@langchain/langgraph';
 
-export interface AttemptRecord {
-  continuation: string;
-  score: number;
-  feedback: string;
-}
-
 export const WritingState = Annotation.Root({
   inputText: Annotation<string>({
     reducer: (a, b) => b ?? a,
@@ -14,30 +8,6 @@ export const WritingState = Annotation.Root({
   continuation: Annotation<string>({
     reducer: (a, b) => b ?? a,
     default: () => '',
-  }),
-  evaluationScore: Annotation<number>({
-    reducer: (a, b) => b ?? a,
-    default: () => 0,
-  }),
-  evaluationFeedback: Annotation<string>({
-    reducer: (a, b) => b ?? a,
-    default: () => '',
-  }),
-  passed: Annotation<boolean>({
-    reducer: (a, b) => b ?? a,
-    default: () => false,
-  }),
-  iteration: Annotation<number>({
-    reducer: (a, b) => (b ?? a) + 1,
-    default: () => 0,
-  }),
-  maxIterations: Annotation<number>({
-    reducer: (a, b) => b ?? a,
-    default: () => 3,
-  }),
-  history: Annotation<AttemptRecord[]>({
-    reducer: (a, b) => [...(a ?? []), ...(b ?? [])],
-    default: () => [],
   }),
 });
 
