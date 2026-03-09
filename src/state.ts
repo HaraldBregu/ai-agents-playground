f5ebdce4b2d01d8faa0727dea866378fd75e9590
HaraldBregu <harald.bregu@gmail.com>
2026-03-09 08:43:59 +0100

    update

--- src/state.ts
+++
@@ -1,9 +0,0 @@
-import { Annotation } from '@langchain/langgraph';
-
-export const WriterState = Annotation.Root({
-  inputText: Annotation<string>,
-  instruction: Annotation<string>,
-  generatedText: Annotation<string>,
-});
-
-export type WriterStateValue = typeof WriterState.State;
