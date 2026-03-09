ae385693de4465044f7874bff1bdc76a4118da54
HaraldBregu <harald.bregu@gmail.com>
2026-03-09 11:55:44 +0100

    Replace evaluator with grammar node and simplify continuation graph

--- playground/text_continuation_evaluator/graph.ts
+++ playground/text_continuation_evaluator/graph.ts
@@ -1,6 +1,6 @@
 import { Annotation, StateGraph } from '@langchain/langgraph';
 import { writerNode } from './writer-node';
-import { evaluatorNode } from './evaluator-node';
+import { grammarNode } from './grammar-node';
 
 export const ContinuationState = Annotation.Root({
   inputText: Annotation<string>({
@@ -11,43 +11,15 @@ export const ContinuationState = Annotation.Root({
     reducer: (_a, b) => b,
     default: () => '',
   }),
-  evaluationScore: Annotation<number>({
-    reducer: (_a, b) => b,
-    default: () => 0,
-  }),
-  evaluationFeedback: Annotation<string>({
-    reducer: (_a, b) => b,
-    default: () => '',
-  }),
-  passed: Annotation<boolean>({
-    reducer: (_a, b) => b,
-    default: () => false,
-  }),
-  iteration: Annotation<number>({
-    reducer: (a, b) => (b ?? a) + 1,
-    default: () => 0,
-  }),
-  maxIterations: Annotation<number>({
-    reducer: (_a, b) => b,
-    default: () => 3,
-  }),
 });
-
-function routeAfterEvaluation(
-  state: typeof ContinuationState.State,
-): 'writer' | '__end__' {
-  if (state.passed) return '__end__';
-  if (state.iteration >= state.maxIterations) return '__end__';
-  return 'writer';
-}
 
 export function createContinuationGraph() {
   const graph = new StateGraph(ContinuationState)
     .addNode('writer', writerNode)
-    .addNode('evaluator', evaluatorNode)
+    .addNode('grammar', grammarNode)
     .addEdge('__start__', 'writer')
-    .addEdge('writer', 'evaluator')
-    .addConditionalEdges('evaluator', routeAfterEvaluation);
+    .addEdge('writer', 'grammar')
+    .addEdge('grammar', '__end__');
 
   return graph.compile();
 }
