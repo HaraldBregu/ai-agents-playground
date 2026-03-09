ca290e0c47336539e614a9f0f4dd87487f64c5e4
harald bregu <harald.bregu@gmail.com>
2026-03-09 22:04:01 +0100

    Add content enhancer agent graph with state and enhancement levels

---
+++ playground/content_enhancer_agent/graph.ts
@@ -0,0 +1,30 @@
+import { Annotation, StateGraph } from '@langchain/langgraph';
+import { enhanceContentNode } from './node';
+
+export const EnhancerState = Annotation.Root({
+  inputText: Annotation<string>({
+    reducer: (_a, b) => b,
+    default: () => '',
+  }),
+  content: Annotation<string>({
+    reducer: (_a, b) => b,
+    default: () => '',
+  }),
+  enhancementLevel: Annotation<'light' | 'moderate' | 'heavy'>({
+    reducer: (_a, b) => b,
+    default: () => 'light',
+  }),
+  completion: Annotation<string>({
+    reducer: (_a, b) => b,
+    default: () => '',
+  }),
+});
+
+export function createEnhancerGraph() {
+  const graph = new StateGraph(EnhancerState)
+    .addNode('enhance_content', enhanceContentNode)
+    .addEdge('__start__', 'enhance_content')
+    .addEdge('enhance_content', '__end__');
+
+  return graph.compile();
+}
