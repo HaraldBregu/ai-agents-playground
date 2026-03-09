4c27a7e97f238d4d9ae9c333e7488835923376ee
HaraldBregu <harald.bregu@gmail.com>
2026-03-09 14:16:05 +0100

    Add content writer agent graph state and intent node wiring

--- playground/content_writer_agent/graph.ts
+++ playground/content_writer_agent/graph.ts
@@ -1,10 +1,23 @@
 import { Annotation, StateGraph } from '@langchain/langgraph';
+import { intentNode } from './intent-node';
 import { writerNode } from './writer-node';
 
 export const WriterState = Annotation.Root({
   inputText: Annotation<string>({
     reducer: (_a, b) => b,
     default: () => '',
+  }),
+  intent: Annotation<string>({
+    reducer: (_a, b) => b,
+    default: () => '',
+  }),
+  content: Annotation<string>({
+    reducer: (_a, b) => b,
+    default: () => '',
+  }),
+  constraints: Annotation<string | null>({
+    reducer: (_a, b) => b,
+    default: () => null,
   }),
   completion: Annotation<string>({
     reducer: (_a, b) => b,
@@ -14,8 +27,10 @@ export const WriterState = Annotation.Root({
 
 export function createWriterGraph() {
   const graph = new StateGraph(WriterState)
+    .addNode('intent', intentNode)
     .addNode('writer', writerNode)
-    .addEdge('__start__', 'writer')
+    .addEdge('__start__', 'intent')
+    .addEdge('intent', 'writer')
     .addEdge('writer', '__end__');
 
   return graph.compile();
